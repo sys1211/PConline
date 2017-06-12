a64cc2c6d0e5b3243b32a9af11e6bac526057300
leiyaqiang <leiyaqiang0120@outlook.com>
2017-06-12 10:29:18 +0800

    lyq

---
+++ app/src/main/java/com/example/tf/pconline/Tools/Cans.java
@@ -0,0 +1,168 @@
+package com.example.tf.pconline.Tools;
+
+/**
+ *
+ */
+class Cans {
+    /*资讯 头条*/
+    public static final String INFORMATION_TOPLINES = "http://mrobot.pconline.com.cn/v2/cms/channels/999?pageNo=";
+    /*资讯 图赏*/
+    public static final String INFORMATION_PICLOOK = "http://mrobot.pconline.com.cn/v2/cms/channels/11?pageSize=20&pageNo=";
+
+    /*资讯 新闻*/
+    public static final String INFORMATION_NEWS = "http://mrobot.pconline.com.cn/v2/cms/channels/";
+    public static final String INFORMATION_NEWS1 = "?pageNo=";
+    public static final String INFORMATION_NEWS2 = "&pageSize=20&appVersion=4.5.2";
+    //2新闻
+    //
+    /*资讯 测评*/
+    public static final String INFORMATION_EVALUATION = "http://mrobot.pconline.com.cn/v2/cms/channels/4?pageNo=";
+    public static final String INFORMATION_EVALUATION2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 视听*/
+    public static final String INFORMATION_LISTEN = "http://mrobot.pconline.com.cn/v2/cms/channels/12?pageNo=";
+    public static final String INFORMATION_LISTEN2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 汽车科技  与手机都是同一种类型*/
+    public static final String INFORMATION_CAR = "http://mrobot.pconline.com.cn/v2/cms/channels/17?pageNo=";
+    public static final String INFORMATION_CAR2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 inLife  与手机是同一种类型*/
+    public static final String INFORMATION_INLIFE = "http://mrobot.pconline.com.cn/v2/cms/channels/14?pageNo=";
+    public static final String INFORMATION_INLIFE2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 软件  与手机是同一种类型  从第二条数据开始*/
+    public static final String INFORMATION_APP = "http://mrobot.pconline.com.cn/v2/cms/channels/1000?pageNo=";
+    public static final String INFORMATION_APP2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 笔记本  从第二条数据开始*/
+    public static final String INFORMATION_NOTOBOOK = "http://mrobot.pconline.com.cn/v2/cms/channels/300?pageNo=";
+    public static final String INFORMATION_NOTOBOOK2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 平板 与手机都是同一种类型*/
+    public static final String INFORMATION_TABLET = "http://mrobot.pconline.com.cn/v2/cms/channels/400?pageNo=";
+    public static final String INFORMATION_TABLET2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 DIY外设 与手机是同一种类型*/
+    public static final String INFORMATION_DIY = "http://mrobot.pconline.com.cn/v2/cms/channels/120?pageNo=";
+    public static final String INFORMATION_DIY2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 相机 与手机是同一种类型*/
+    public static final String INFORMATION_CAMERA = "http://mrobot.pconline.com.cn/v2/cms/channels/500?pageNo=";
+    public static final String INFORMATION_CAMERA2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 无人机 与手机是同一种类型 */
+    public static final String INFORMATION_UAV = "http://mrobot.pconline.com.cn/v2/cms/channels/13?pageNo=";
+    public static final String INFORMATION_UAV2 = "&pageSize=20&appVersion=4.5.2";
+
+    /**
+     * 智能硬件
+     */
+    public static final String INFORMATION_HARDWARE = "http://mrobot.pconline.com.cn/v2/cms/channels/501?pageNo=";
+    public static final String INFORMATION_HARDWARE2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 智能家电  与手机是同一种类型*/
+    public static final String INFORMATION_HOMEAPP = "http://mrobot.pconline.com.cn/v2/cms/channels/600?pageNo=";
+    public static final String INFORMATION_HOMEAPP2 = "&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 企业圈  与手机是同一种类型*/
+    public static final String INFORMATION_BUSINESS = "http://mrobot.pconline.com.cn/v2/cms/channels/15?pageNo=1&pageSize=20&appVersion=4.5.2";
+
+    /*资讯 网络图  与手机是同一种类型*/
+    public static final String INFORMATION_INTENET = "http://mrobot.pconline.com.cn/v2/cms/channels/16?pageNo=1&pageSize=20&appVersion=4.5.2";
+    /*资讯 导购  与手机是同一种类型 */
+    public static final String INFORMATION_SHOPPER = "http://mrobot.pconline.com.cn/v2/cms/channels/3?pageNo=1&pageSize=20&appVersion=4.5.2";
+
+
+    /*论坛 列表*/
+    public static final String BBS_LIST = "http://mrobot.pconline.com.cn/configs/pconline_v4_bbs_forum_tree";
+    /*论坛广场  顶部轮播图片*/
+    public static final String BBS_TOPICS = "http://mrobot.pconline.com.cn/s-900/onlineinfo/bbs/forumFocus.xsp";
+    /*论坛广场   顶部轮播图片详情*/
+    public static final String BBS_TOPICS_DETAIL = "http://mrobot.pconline.com.cn/v2/itbbs/topics/";
+    /*论坛 广场列表 */
+    public static final String BBS_SQUARE_LIST = "http://mrobot.pconline.com.cn/s-900/onlineinfo/bbs/topics.xsp?type=hot_day&forums=";
+    /*论坛列表  gridview手机*/
+    public static final String BBS_SQUARE_PHONE = "http://mrobot.pconline.com.cn/s-900/onlineinfo/bbs/topics.xsp?singleForum=false&ie=utf-8&count=20&showImage=true&type=hot_week&forums=";
+    public static final String BBS_TOPPICLIST = "http://mrobot.pconline.com.cn/s/onlineinfo/bbs/forumsItbbs.xsp?forumId=";
+
+    /* 直播*/
+    public static final String INFORMATION_LIVE = "http://mrobot.pconline.com.cn/v2/cms/channels/10?pageNo=";
+
+    /*找产品  手机*/
+    public static final String LOOK_PRODUCT_PHONE = "http://mrobot.pconline.com.cn/v3/product/brand/";
+
+    /*找产品  相机*/
+    public static final String LOOK_PRODUCT_XIANGJI = "http://mrobot.pconline.com.cn/v3/product/brand/20928";
+
+    /*找产品  笔记本*/
+    public static final String LOOK_PRODUCT_NOTEBOOK = "http://mrobot.pconline.com.cn/v3/product/brand/20807";
+
+    /*找产品  DIY硬件*/
+
+    /*找产品  超极本*/
+    public static final String LOOK_PRODUCT_SUPER_NOTEBOOK = "http://mrobot.pconline.com.cn/s/onlineinfo/product/brandList.xsp?typeId=95585";
+
+    /*找产品  平板电脑*/
+    public static final String LOOK_PRODUCT_TABLET = "http://mrobot.pconline.com.cn/v3/product/brand/79849";
+
+    /*找产品  摄像机*/
+    public static final String LOOK_PRODUCT_SHEXIANGJI = "http://mrobot.pconline.com.cn/v3/product/brand/20912";
+
+    //任意品牌产品列表    //参数解释    不同品类    不同品牌需要传入id==brandId
+    public static final String PRODUCTS_LIST = "http://mrobot.pconline.com.cn/v3/product/types/";
+
+    /**
+     * 全部品牌列表
+     */
+    public static final String ALL_BRANDLIST = "http://mrobot.pconline.com.cn/s/onlineinfo/product/modelSearch.xsp?smallType=20937&pageNo=";
+
+
+    /**
+     * 产品详情  概述
+     */
+    public static final String GAISHU_PRODUCT = "http://mrobot.pconline.com.cn/s-604800/onlineinfo/product/overview.xsp?modelId=";
+    public static final String GAISHU_PRODUCT2 = "&ver=33&dev_id=4ae4b86eea633f5684e60e5ab25dbc43";
+
+    /**
+     * 产品详情 参数
+     */
+    public static final String CANSHU_PRODUCT = "http://mrobot.pconline.com.cn/v3/product/detail/";
+    public static final String CANSHU_PRODUCT2 = "?ver=33&dev_id=4ae4b86eea633f5684e60e5ab25dbc43";
+    /**
+     * 产品详情  报价
+     */
+    public static final String BAOJIA_PRODUCT = "http://mrobot.pconline.com.cn/s-86400/onlineinfo/product/comparePrice.xsp?modelId=";
+    public static final String BAOJIA_PRODUCT2 = "&dev_id=4ae4b86eea633f5684e60e5ab25dbc43";
+
+    /**
+     * 产品详情 资讯
+     */
+    public static final String ZIXUN_PRODUCT = "http://mrobot.pconline.com.cn/v3/product/information/";
+    public static final String ZIXUN_PRODUCT2 = "?ver=33&pageNo=1&pageSize=20";
+    /**
+     * 产品详情  点评
+     */
+    public static final String DIANPING_PRODUCT = "http://mrobot.pconline.com.cn/v3/product/commentList/";
+
+
+    /**
+     * 产品搜索
+     */
+    public static final  String SOUSUO = "http://g.pconline.com.cn/best/infoapp/discovery.jsp";
+
+
+    public static final String DIANPING_PRODUCT2 = "?ver=33&dev_id=4ae4b86eea633f5684e60e5ab25dbc43";
+    /*聚超值  最新*/
+    public static final String PLOY_NEW = "http://g.pconline.com.cn/best/infoapp/index.jsp";
+    /*聚超值  海淘*/
+    public static final String PLOY_SEA = "http://g.pconline.com.cn/best/infoapp/haitao.jsp";
+    /*聚超值  发现*/
+    public static final String PLOY_FIND = "http://g.pconline.com.cn/best/infoapp/discovery.jsp";
+    /*聚超值  晒物*/
+    public static final String PLOY_SHAIWU = "http://g.pconline.com.cn/best/infoapp/shaiwu.jsp";
+    /*聚超值  经验*/
+    public static final String PLOY_EXP = "http://g.pconline.com.cn/best/infoapp/experience.jsp";
+    /*资讯  图赏详情*/
+    public static final String PICTURE_DETAILS=  "http://mrobot.pconline.com.cn/v2/photo/albums?modelId=";
+}
